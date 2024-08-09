5f8f667c8e2057262c40b71400b4ce985f725388
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-08-09 12:19:04 +0300

    syntax fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // const express = require('express');
 import express from 'express';
-import routes from './routes/index.js';
+import routes from './routes/index';
+// const router = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,6 +12,5 @@ app.use('/', routes);
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
-
